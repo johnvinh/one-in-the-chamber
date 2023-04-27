ad394d4c44b58b8575743b6da33e2980af601e2f
John Vinh <john.michael.vinh2@gmail.com>
2023-04-27 19:12:33 -0400

    Initialize project and add Kotlin to gradle dependencies

---
+++ src/main/java/dev/johnvinh/oneinthechamber/OneInTheChamber.java
@@ -0,0 +1,17 @@
+package dev.johnvinh.oneinthechamber;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OneInTheChamber extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
