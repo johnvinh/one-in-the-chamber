ae66a312d0370019671af3e38385927a137111ce
John Vinh <john.michael.vinh2@gmail.com>
2023-04-27 19:17:09 -0400

    Switch main class to use Kotlin

---
+++ src/main/kotlin/dev/johnvinh/oneinthechamber/OneInTheChamber.kt
@@ -0,0 +1,13 @@
+package dev.johnvinh.oneinthechamber
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class OneInTheChamber : JavaPlugin() {
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
