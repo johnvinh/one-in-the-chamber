ac68728315a56eac9e64444a583a309097ae6f97
John Vinh <john.michael.vinh2@gmail.com>
2023-04-28 11:11:16 -0400

    Register event listener

--- src/main/kotlin/dev/johnvinh/oneinthechamber/OneInTheChamber.kt
+++ src/main/kotlin/dev/johnvinh/oneinthechamber/OneInTheChamber.kt
@@ -1,6 +1,8 @@
 package dev.johnvinh.oneinthechamber
 
 import dev.johnvinh.oneinthechamber.arena.ArenaManager
+import dev.johnvinh.oneinthechamber.game.GameListener
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class InvalidConfigurationException: Exception()
@@ -9,6 +11,7 @@ class OneInTheChamber : JavaPlugin() {
     val arenaManager = ArenaManager(this)
     override fun onEnable() {
         ConfigManager.setupConfig(this)
+        Bukkit.getPluginManager().registerEvents(GameListener(this), this)
     }
 
     override fun onDisable() {
