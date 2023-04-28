78655f88680a6d7a8d360cdb68f8924ac8f6da38
John Vinh <john.michael.vinh2@gmail.com>
2023-04-27 21:25:20 -0400

    Add helper to get the arena a player is in

--- src/main/kotlin/dev/johnvinh/oneinthechamber/arena/ArenaManager.kt
+++ src/main/kotlin/dev/johnvinh/oneinthechamber/arena/ArenaManager.kt
@@ -1,9 +1,9 @@
 package dev.johnvinh.oneinthechamber.arena
 
 import dev.johnvinh.oneinthechamber.OneInTheChamber
-import dev.johnvinh.oneinthechamber.arena.Arena
 import dev.johnvinh.oneinthechamber.copyWorld
 import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import java.io.File
 import java.io.IOException
 import java.util.concurrent.atomic.AtomicInteger
@@ -25,5 +25,18 @@ class ArenaManager(private val plugin: OneInTheChamber) {
         val world = Bukkit.getWorld("oinc-$instanceId") ?: throw IOException("Error retrieving created world")
 
         arenas.add(Arena(plugin, world))
+    }
+
+    /**
+     * Gets the arena a player is playing in.
+     * @param player the player to check
+     */
+    fun getArena(player: Player): Arena? {
+        for (arena in arenas) {
+            if (arena.players.contains(player.uniqueId)) {
+                return arena
+            }
+        }
+        return null
     }
 }
