026534963569b36fe516d7e5031a7c760c8d29ec
John Vinh <john.michael.vinh2@gmail.com>
2023-04-28 11:06:06 -0400

    Implement melee kill point increment

--- src/main/kotlin/dev/johnvinh/oneinthechamber/game/GameListener.kt
+++ src/main/kotlin/dev/johnvinh/oneinthechamber/game/GameListener.kt
@@ -6,14 +6,19 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerPickupArrowEvent
 
 class GameListener(private val plugin: OneInTheChamber) : Listener {
+    /**
+     * Kill a player instantly with a bow shot and increment point
+     */
     @EventHandler
     fun onEntityDamageByEntity(event: EntityDamageByEntityEvent) {
         val damaged = event.entity
         val damager = event.damager
 
+        // One shot bow kills
         if (damaged is Player && damager is Arrow) {
             val shooter = damager.shooter
 
@@ -22,6 +27,30 @@ class GameListener(private val plugin: OneInTheChamber) : Listener {
                 val arena = plugin.arenaManager.getArena(shooter) ?: return
                 arena.game.addKill(shooter)
             }
+        }
+    }
+
+    /**
+     * Increment point on a melee kill
+     */
+    @EventHandler
+    fun onPlayerKill(event: PlayerDeathEvent) {
+        val victim = event.entity
+        val killer = victim.killer ?: return
+
+        val arena = plugin.arenaManager.getArena(killer) ?: return
+        arena.game.addKill(killer)
+    }
+
+    /**
+     * Prevent players from picking up arrows
+     */
+    @EventHandler
+    fun onItemPickup(event: PlayerPickupArrowEvent) {
+        val player = event.player
+        val arena = plugin.arenaManager.getArena(player) ?: return
+        if (arena.state == ArenaState.LIVE) {
+            event.isCancelled = true
         }
     }
 }
